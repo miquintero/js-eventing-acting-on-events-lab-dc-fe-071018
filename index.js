d7ee6110d19b59d96de4af2891e64310e6bc511f
Alexa Schaeffer <alps231@gmail.com>
2018-08-26 14:11:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var dodger = document.getElementById('dodger')
 
-function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
 
   if (left > 0) {
     dodger.style.left = `${left - 1}px`
